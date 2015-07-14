6edea1b381dbd30c272bb1abd838b8b99939d9cc
flameshimmer <nm1072@gmail.com>
2015-07-13 19:06:20 -0700

    P

--- Leet2015_2/Leet2015_2/PalindromePartitioning.cpp
+++ Leet2015_2/Leet2015_2/PalindromePartitioning.cpp
@@ -16,8 +16,38 @@ namespace Solution2
 {
     namespace PalindromePartitioning
     {
-     
-     
+		bool isPalindrome(string& s, int start, int end)
+		{
+			while (start < end)
+			{
+				if (s[start] != s[end]) { return false; }
+				start++;
+				end--;
+			}
+			return true;
+		}
+
+
+		void partition(string& s, int start, vector<string>& result, vector<vector<string>>& results)
+		{
+			if (start == s.length()) 
+			{
+				results.push_back(result);
+				return;
+			}
+
+
+		}
+
+		vector<vector<string>> partition(string s) 
+		{
+			vector<vector<string>> results;
+			int len = s.length();
+			if (len == 0) { return results; }
+			vector<string> result;
+			vector<vector<bool>> P()
+
+		}
      
      
         void Main()
--- Leet2015_2/Leet2015_2/PalindromePartitioningII.cpp
+++ Leet2015_2/Leet2015_2/PalindromePartitioningII.cpp
@@ -1,13 +1,21 @@
 #include "stdafx.h"
 
-// 
+//Given a string s, partition s such that every substring of the partition is a palindrome.
+//
+//Return the minimum cuts needed for a palindrome partitioning of s.
+//
+//For example, given s = "aab",
+//Return 1 since the palindrome partitioning["aa", "b"] could be produced using 1 cut.
+//
+
 namespace Solution2
 {
     namespace PalindromePartitioningII
     {
      
-     
-     
+		int minCut(string s) {
+
+		}
      
         void Main()
         {
