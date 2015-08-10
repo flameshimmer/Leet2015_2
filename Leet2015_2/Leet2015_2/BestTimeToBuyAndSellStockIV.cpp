c844a9d2e33d126f43ea502610ea0aad7758b493
flameshimmer <nm1072@gmail.com>
2015-08-10 10:27:49 -0700

    B

--- Leet2015_2/Leet2015_2/BestTimeToBuyAndSellStockIV.cpp
+++ Leet2015_2/Leet2015_2/BestTimeToBuyAndSellStockIV.cpp
@@ -15,26 +15,26 @@ namespace Solution2
 			int len = prices.size();
 			if (len < 2) { return 0; }
 
-			if (k > len)
+			if (k >  len)
 			{
 				int result = 0;
-				for (int i = 1; i < len; i++)
+				for (int i = 1; i<len; i++)
 				{
 					result += max(0, prices[i] - prices[i - 1]);
 				}
 				return result;
 			}
 
-			vector<int> max_local(k+1, 0);
-			vector<int> max_global(k+1, 0);
+			vector<int> max_local(k + 1, 0);
+			vector<int> max_global(k + 1, 0);
 
-			for (int i = 1; i < len; i++)
+			for (int i = 1; i<len; i++)
 			{
 				int diff = prices[i] - prices[i - 1];
-				for (int j = k; j >=1; j--)
+				for (int j = k; j >= 1; j--)
 				{
-					max_local[j] = max(max_global[j - 1] + max(0, diff), max_local[j - 1]);
-					max_global[j] = max(max_local[j], max_global[j]);
+					max_local[j] = max(max_global[j - 1] + max(0, diff), max_local[j] + diff);
+					max_global[j] = max(max_global[j], max_local[j]);
 				}
 			}
 			return max_global[k];
