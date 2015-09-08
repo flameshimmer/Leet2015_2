724aab41289143c1c9bcbe209f7417eee316d146
flameshimmer <nm1072@gmail.com>
2015-09-08 14:02:25 -0700

    update wildcard

--- Leet2015_2/Leet2015_2/WildcardMatching.cpp
+++ Leet2015_2/Leet2015_2/WildcardMatching.cpp
@@ -41,6 +41,33 @@ namespace Solution2
 		bool isMatch(string s, string p) {
 			return isMatch(s.c_str(), p.c_str());
 		}
+
+		namespace DP
+		{
+			bool isMatch(string s, string p) 
+			{
+				int lens = s.length();
+				int lenp = p.length();
+				
+				vector<vector<bool>> M(lens + 1, vector<bool>(lenp + 1));
+				M[0][0] = true;
+
+				for (int i = 1; i < lenp + 1 && p[i-1] == '*'; i++)
+				{
+					M[0][i] = true;
+				}
+
+				for (int i = 1; i < lens + 1; i++)
+				{
+					for (int j = 1; j < lenp + 1; j++)
+					{
+						if (s[i - 1] == p[j - 1] || p[j-1] == '?') { M[i][j] = M[i - 1][j - 1]; }
+						else if (p[j - 1] == '*') { M[i][j] = M[i - 1][j] || M[i][j - 1]; }
+					}
+				}
+				return M[lens][lenp];
+			}
+		}
      
         void Main()
         {
